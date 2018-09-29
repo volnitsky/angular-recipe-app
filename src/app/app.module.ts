260a46c69348a0755880808e9a4a316cabc3d057
Dmitry Volnitsky <dmitry@volnitsky.com>
2018-09-30 02:37:49 +0300

    add recipies component and shoping list + add css framework bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { RecipiesComponent } from './recipies/recipies.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { RecipeDetailComponent } from './recipies/recipe-detail/recipe-detail.component';
+import { RecipeListComponent } from './recipies/recipe-list/recipe-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RecipiesComponent
+    RecipiesComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
+    RecipeDetailComponent,
+    RecipeListComponent
   ],
   imports: [
     BrowserModule
