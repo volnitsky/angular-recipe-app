958d85a1cd35e4b0b7606fc8362d524fc6012c39
dmitryV <dmitry@volnitsky.com>
2018-10-05 03:14:53 +0300

    refactor ricipe-list and recipe-item component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,8 +1,8 @@
-import { Component, Output, EventEmitter } from "@angular/core";
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
-  selector: "app-header",
-  templateUrl: "./header.component.html"
+  selector: 'app-header',
+  templateUrl: './header.component.html'
 })
 export class HeaderComponent {
   @Output()
--- src/app/recipies/recipies.component.ts
+++ src/app/recipies/recipies.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: "app-recipes",
-  templateUrl: "./recipies.component.html",
-  styleUrls: ["./recipies.component.css"]
+  selector: 'app-recipes',
+  templateUrl: './recipies.component.html',
+  styleUrls: ['./recipies.component.css']
 })
 export class RecipiesComponent implements OnInit {
   constructor() {}
